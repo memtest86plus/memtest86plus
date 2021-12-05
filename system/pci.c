a7d17186f3eb8ad58fe654bfd3529b1f0bd3c488
Martin Whitaker <pcmemtest@martin-whitaker.me.uk>
2021-12-05 14:15:37 +0000

    Improved PCI configuration space access functions.

--- system/pci.c
+++ system/pci.c
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0
-// Copyright (C) 2020 Martin Whitaker.
+// Copyright (C) 2020-2021 Martin Whitaker.
 //
 // Derived from memtest86+ pci.c:
 //
@@ -12,7 +12,11 @@
 // Released under version 2 of the Gnu Public License.
 // By Chris Brady
 
+#include <stdbool.h>
 #include <stdint.h>
+
+#include "boot.h"
+#include "bootparams.h"
 
 #include "cpuid.h"
 #include "io.h"
@@ -23,171 +27,219 @@
 // Constants
 //------------------------------------------------------------------------------
 
-#define PCI_CONF_TYPE_NONE      0
-#define PCI_CONF_TYPE_1         1
-#define PCI_CONF_TYPE_2         2
-
 #define PCI_CLASS_DEVICE        0x0a
 
 #define PCI_CLASS_BRIDGE_HOST   0x0600
 
 //------------------------------------------------------------------------------
+// Types
+//------------------------------------------------------------------------------
+
+typedef enum {
+    PCI_CONFIG_TYPE_NONE  = 0,
+    PCI_CONFIG_TYPE_1     = 1,
+    PCI_CONFIG_TYPE_2     = 2
+} pci_config_type_t;
+
+//------------------------------------------------------------------------------
 // Private Variables
 //------------------------------------------------------------------------------
 
-static unsigned char pci_conf_type = PCI_CONF_TYPE_NONE;
+static pci_config_type_t pci_config_type = PCI_CONFIG_TYPE_NONE;
 
 //------------------------------------------------------------------------------
 // Private Functions
 //------------------------------------------------------------------------------
 
-#define PCI_CONF1_ADDRESS(bus, dev, fn, reg) \
-    (0x80000000 | (bus << 16) | (dev << 11) | (fn << 8) | (reg & ~3))
-
-#define PCI_CONF2_ADDRESS(dev, reg)    (unsigned short)(0xC000 | (dev << 8) | reg)
-
-#define PCI_CONF3_ADDRESS(bus, dev, fn, reg) \
-    (0x80000000 | (((reg >> 8) & 0xF) << 24) | (bus << 16) | ((dev & 0x1F) << 11) | (fn << 8) | (reg & 0xFF))
-
-static int pci_sanity_check(void)
+static bool pci_sanity_check(void)
 {
-    /* Do a trivial check to make certain we can see a host bridge.
-     * There are reportedly some buggy chipsets from intel and
-     * compaq where this test does not work, I will worry about
-     * that when we support them.
-     */
-    uint32_t value;
-    int result = pci_conf_read(0, 0, 0, PCI_CLASS_DEVICE, 2, &value);
-    if (result == 0) {
-        result = -1;
-        if (value == PCI_CLASS_BRIDGE_HOST) {
-            result = 0;
-        }
-    }
-    return result;
+    // Do a trivial check to make certain we can see a host bridge.
+    // There are reportedly some buggy chipsets from Intel and
+    // Compaq where this test does not work, I will worry about
+    // that when we support them.
+    return pci_config_read16(0, 0, 0, PCI_CLASS_DEVICE) == PCI_CLASS_BRIDGE_HOST;
 }
 
-static int pci_check_direct(void)
+static void probe_config_type(void)
 {
-    unsigned char tmpCFB;
-    unsigned int  tmpCF8;
+    uint8_t  tmpCFB;
+    uint32_t tmpCF8;
 
-    if (cpuid_info.vendor_id.str[0] == 'A' && cpuid_info.version.family == 0xF) {
-        pci_conf_type = PCI_CONF_TYPE_1;
-        return 0;
-    } else {
-        /* Check if configuration type 1 works. */
-        pci_conf_type = PCI_CONF_TYPE_1;
-        tmpCFB = inb(0xCFB);
-        outb(0x01, 0xCFB);
-        tmpCF8 = inl(0xCF8);
-        outl(0x80000000, 0xCF8);
-        if ((inl(0xCF8) == 0x80000000) && (pci_sanity_check() == 0)) {
-            outl(tmpCF8, 0xCF8);
-            outb(tmpCFB, 0xCFB);
-            return 0;
-        }
-        outl(tmpCF8, 0xCF8);
-
-        /* Check if configuration type 2 works. */
-
-        pci_conf_type = PCI_CONF_TYPE_2;
-        outb(0x00, 0xCFB);
-        outb(0x00, 0xCF8);
-        outb(0x00, 0xCFA);
-        if (inb(0xCF8) == 0x00 && inb(0xCFA) == 0x00 && (pci_sanity_check() == 0)) {
-            outb(tmpCFB, 0xCFB);
-            return 0;
-        }
-
-        outb(tmpCFB, 0xCFB);
-
-        /* Nothing worked return an error */
-        pci_conf_type = PCI_CONF_TYPE_NONE;
-        return -1;
+    if (cpuid_info.vendor_id.str[0] == 'A' && cpuid_info.version.family == 0xf) {
+        pci_config_type = PCI_CONFIG_TYPE_1;
+        return;
     }
+
+    // Check if configuration type 1 works.
+    pci_config_type = PCI_CONFIG_TYPE_1;
+    tmpCFB = inb(0xcfb);
+    outb(0x01, 0xcfb);
+    tmpCF8 = inl(0xcf8);
+    outl(0x80000000, 0xcf8);
+    if (inl(0xcf8) == 0x80000000 && pci_sanity_check()) {
+        outl(tmpCF8, 0xcf8);
+        outb(tmpCFB, 0xcfb);
+        return;
+    }
+    outl(tmpCF8, 0xcf8);
+
+    // Check if configuration type 2 works.
+    pci_config_type = PCI_CONFIG_TYPE_2;
+    outb(0x00, 0xcfb);
+    outb(0x00, 0xcf8);
+    outb(0x00, 0xcfa);
+    if (inb(0xcf8) == 0x00 && inb(0xcfa) == 0x00 && pci_sanity_check()) {
+        outb(tmpCFB, 0xcfb);
+        return;
+    }
+    outb(tmpCFB, 0xcfb);
+
+    // Nothing worked.
+    pci_config_type = PCI_CONFIG_TYPE_NONE;
+}
+
+static void set_pci_config1_addr(int bus, int dev, int func, int reg)
+{
+    uint32_t addr = 0x80000000
+                  | (reg  & 0xf00) << 16
+                  | (bus  & 0xff)  << 16
+                  | (dev  & 0x1f)  << 11
+                  | (func & 0x07)  << 8
+                  | (reg  & 0xfc);
+
+    outl(addr, 0xcf8);
+}
+
+static void set_pci_config2_bus_func(int bus, int func)
+{
+    outb(0xf0 | (func & 0x7) << 1, 0xcf8);
+    outb(bus, 0xcfa);
+}
+
+static int pci_config2_access_addr(int dev, int reg)
+{
+    return 0xc000 | (dev & 0x1f) << 8 | (reg & 0xff);
 }
 
 //------------------------------------------------------------------------------
 // Public Functions
 //------------------------------------------------------------------------------
 
-int pci_init(void)
+void pci_init(void)
 {
-    // For now just make certain we can directly use the pci functions.
-    return pci_check_direct();
-}
-
-int pci_conf_read(unsigned bus, unsigned dev, unsigned fn, unsigned reg, unsigned len, uint32_t *value)
-{
-    int result;
-
-    if (!value || (bus > 255) || (dev > 31) || (fn > 7) || (reg > 255 && pci_conf_type != PCI_CONF_TYPE_1))
-        return -1;
-
-    result = -1;
-    switch (pci_conf_type) {
-      case PCI_CONF_TYPE_1:
-        if (reg < 256) {
-            outl(PCI_CONF1_ADDRESS(bus, dev, fn, reg), 0xCF8);
-        } else {
-            outl(PCI_CONF3_ADDRESS(bus, dev, fn, reg), 0xCF8);
-        }
-        switch (len) {
-          case 1:  *value = inb(0xCFC + (reg & 3)); result = 0; break;
-          case 2:  *value = inw(0xCFC + (reg & 2)); result = 0; break;
-          case 4:  *value = inl(0xCFC); result = 0; break;
-        }
-        break;
-      case PCI_CONF_TYPE_2:
-        outb(0xF0 | (fn << 1), 0xCF8);
-        outb(bus, 0xCFA);
-
-        switch (len) {
-          case 1:  *value = inb(PCI_CONF2_ADDRESS(dev, reg)); result = 0; break;
-          case 2:  *value = inw(PCI_CONF2_ADDRESS(dev, reg)); result = 0; break;
-          case 4:  *value = inl(PCI_CONF2_ADDRESS(dev, reg)); result = 0; break;
-        }
-        outb(0, 0xCF8);
-        break;
+    const boot_params_t *boot_params = (boot_params_t *)boot_params_addr;
+    if (boot_params->efi_info.loader_signature) {
+        // On UEFI systems we can assume configuration type 1.
+        pci_config_type = PCI_CONFIG_TYPE_1;
+    } else {
+        probe_config_type();
     }
-    return result;
 }
 
-int pci_conf_write(unsigned bus, unsigned dev, unsigned fn, unsigned reg, unsigned len, uint32_t value)
+uint8_t pci_config_read8(int bus, int dev, int func, int reg)
 {
-    int result;
+    uint8_t value;
 
-    if (!value || (bus > 255) || (dev > 31) || (fn > 7) || (reg > 255 && pci_conf_type != PCI_CONF_TYPE_1))
-        return -1;
+    switch (pci_config_type) {
+      case PCI_CONFIG_TYPE_1:
+        set_pci_config1_addr(bus, dev, func, reg);
+        return inb(0xcfc + (reg & 0x3));
+      case PCI_CONFIG_TYPE_2:
+        set_pci_config2_bus_func(bus, func);
+        value = inb(pci_config2_access_addr(dev, reg));
+        outb(0, 0xcf8);
+        return value;
+      default:
+        return 0xFF;
+    }
+}
 
-    result = -1;
+uint16_t pci_config_read16(int bus, int dev, int func, int reg)
+{
+    uint16_t value;
 
-    switch (pci_conf_type)
+    switch (pci_config_type) {
+      case PCI_CONFIG_TYPE_1:
+        set_pci_config1_addr(bus, dev, func, reg);
+        return inw(0xcfc + (reg & 0x2));
+      case PCI_CONFIG_TYPE_2:
+        set_pci_config2_bus_func(bus, func);
+        value = inw(pci_config2_access_addr(dev, reg));
+        outb(0, 0xcf8);
+        return value;
+      default:
+        return 0xFFFF;
+    }
+}
+
+uint32_t pci_config_read32(int bus, int dev, int func, int reg)
+{
+    uint32_t value;
+
+    switch (pci_config_type) {
+      case PCI_CONFIG_TYPE_1:
+        set_pci_config1_addr(bus, dev, func, reg);
+        return inl(0xcfc);
+      case PCI_CONFIG_TYPE_2:
+        set_pci_config2_bus_func(bus, func);
+        value = inl(pci_config2_access_addr(dev, reg));
+        outb(0, 0xcf8);
+        return value;
+      default:
+        return 0xFFFFFFFF;
+    }
+}
+
+void pci_config_write8(int bus, int dev, int func, int reg, uint8_t value)
+{
+    switch (pci_config_type)
     {
-      case PCI_CONF_TYPE_1:
-        if (reg < 256) {
-            outl(PCI_CONF1_ADDRESS(bus, dev, fn, reg), 0xCF8);
-        } else {
-            outl(PCI_CONF3_ADDRESS(bus, dev, fn, reg), 0xCF8);
-        }
-        switch (len) {
-          case 1:  outb(value, 0xCFC + (reg & 3)); result = 0; break;
-          case 2:  outw(value, 0xCFC + (reg & 2)); result = 0; break;
-          case 4:  outl(value, 0xCFC); result = 0; break;
-        }
+      case PCI_CONFIG_TYPE_1:
+        set_pci_config1_addr(bus, dev, func, reg);
+        outb(value, 0xcfc + (reg & 0x3));
         break;
-      case PCI_CONF_TYPE_2:
-        outb(0xF0 | (fn << 1), 0xCF8);
-        outb(bus, 0xCFA);
-
-        switch (len) {
-          case 1: outb(value, PCI_CONF2_ADDRESS(dev, reg)); result = 0; break;
-          case 2: outw(value, PCI_CONF2_ADDRESS(dev, reg)); result = 0; break;
-          case 4: outl(value, PCI_CONF2_ADDRESS(dev, reg)); result = 0; break;
-        }
-        outb(0, 0xCF8);
+      case PCI_CONFIG_TYPE_2:
+        set_pci_config2_bus_func(bus, func);
+        outb(value, pci_config2_access_addr(dev, reg));
+        outb(0, 0xcf8);
+        break;
+      default:
         break;
     }
-    return result;
+}
+
+void pci_config_write16(int bus, int dev, int func, int reg, uint16_t value)
+{
+    switch (pci_config_type)
+    {
+      case PCI_CONFIG_TYPE_1:
+        set_pci_config1_addr(bus, dev, func, reg);
+        outw(value, 0xcfc + (reg & 0x2));
+        break;
+      case PCI_CONFIG_TYPE_2:
+        set_pci_config2_bus_func(bus, func);
+        outw(value, pci_config2_access_addr(dev, reg));
+        outb(0, 0xcf8);
+        break;
+      default:
+        break;
+    }
+}
+
+void pci_config_write32(int bus, int dev, int func, int reg, uint32_t value)
+{
+    switch (pci_config_type)
+    {
+      case PCI_CONFIG_TYPE_1:
+        set_pci_config1_addr(bus, dev, func, reg);
+        outl(value, 0xcfc);
+        break;
+      case PCI_CONFIG_TYPE_2:
+        set_pci_config2_bus_func(bus, func);
+        outl(value, pci_config2_access_addr(dev, reg));
+        outb(0, 0xcf8);
+        break;
+      default:
+        break;
+    }
 }
