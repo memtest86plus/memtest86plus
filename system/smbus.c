ee0c400821c28155f1f2a78243529c974a6d9f3f
Sam Demeulemeester <github@x86-secret.com>
2023-02-18 19:01:59 +0100

    [SMBUS] Add support for VIA VT8233 Southbridge

--- system/smbus.c
+++ system/smbus.c
@@ -1251,7 +1251,7 @@ static bool find_smb_controller(uint16_t vid, uint16_t did)
                     // SMB base address = 0x90
                     // viapro SMBus controller, i.e. PIIX4.
                     return piix4_get_smb(PIIX4_SMB_BASE_ADR_DEFAULT);
-                // case 0x3074: // 8233_0
+                case 0x3074: // 8233
                 case 0x3147: // 8233A
                 case 0x3177: // 8235
                 case 0x3227: // 8237
