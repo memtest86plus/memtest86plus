00fc1bb230787d361d2662b6ed980e097b5dbaf5
Sam Demeulemeester <github@x86-secret.com>
2023-02-20 00:05:35 +0100

    First functions skeleton for reading IMC/ECC Registers

---
+++ system/memctrl.h
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: GPL-2.0
+#ifndef MEMCTRL_H
+#define MEMCTRL_H
+/**
+ * \file
+ *
+ * Provides information about the memory controller status
+ * (running DRAM configuration, ECC, ...) and other
+ * platform-specific data
+ *
+ *//*
+ * Copyright (C) 2004-2023 Sam Demeulemeester.
+ */
+
+typedef struct imc_infos {
+    uint16_t    freq;
+    uint16_t    tCL;
+    uint8_t     tCL_dec;
+    uint16_t    tRCD;
+    uint16_t    tRP;
+    uint16_t    tRAS;
+} imc_info_t;
+
+typedef enum {
+    ECC_ERR_NONE,
+    ECC_ERR_CORRECTED,
+    ECC_ERR_UNCORRECTED
+} ecc_error_typo_t;
+
+typedef struct ecc_status {
+    bool                ecc_enabled;
+    ecc_error_typo_t    err_type;
+    uint64_t            err_adr;
+    uint32_t            err_col;
+    uint32_t            err_row;
+    uint32_t            err_rank;
+    uint32_t            err_bank;
+} ecc_info_t;
+
+/**
+ * Current DRAM configuration of the Integrated Memory Controller
+ */
+
+extern imc_info_t imc_status;
+
+/**
+ * Current ECC Status of the Integrated Memory Controller
+ */
+
+extern ecc_info_t ecc_status;
+
+void memctrl_init(void);
+
+#endif // MEMCTRL_H
