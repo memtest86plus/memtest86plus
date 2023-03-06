8c35ea159990b0879957d6c573a009fcc79a92b8
Sam Demeulemeester <github@x86-secret.com>
2023-03-06 22:06:45 +0100

    Add Intel HSW and fix new files encoding

--- system/mch/mch.h
+++ system/mch/mch.h
@@ -1,8 +1,12 @@
 // SPDX-License-Identifier: GPL-2.0
-// Copyright (C) 2004-2022 Sam Demeulemeester
+// Copyright (C) 2004-2023 Sam Demeulemeester
 #ifndef _CHIPSET_H_
 #define _CHIPSET_H_
 
+/* Memory configuration Detection for Intel Haswell */
+void get_imc_config_intel_hsw(void);
+
+/* Memory configuration Detection for Intel Skylake */
 void get_imc_config_intel_skl(void);
 
 #endif /* _CHIPSET_H_ */
