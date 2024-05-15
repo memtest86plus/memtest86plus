8244b5afde3ca1e49e7fadf842834a99ef62d2db
demiurg-spb <ivanov@rigler.ru>
2024-05-15 16:40:37 +0300

    Add beep sound upon pass or failure, based on PIT channel 2.
    Changes by Lionel Debroux:
    * disable sound output by default, and make it possible to enable it, but do intentionally not document the feature;
    * improve commit message;
    * fix data types;
    * replace static const variables by #defines - it's a bit early to use C23 constexpr;
    * make the coding style consistent with the rest of the memtest86+ code base.

--- app/display.c
+++ app/display.c
@@ -28,6 +28,8 @@
 
 #include "tests.h"
 
+#include "sound.h"
+
 #include "display.h"
 
 //------------------------------------------------------------------------------
@@ -397,6 +399,10 @@ void display_big_status(bool pass)
         return;
     }
 
+    if (enable_sound) {
+        sound_beep(pass);
+    }
+
     save_screen_region(POP_STATUS_REGION, popup_status_save_buffer);
 
     set_background_colour(BLACK);
@@ -576,6 +582,9 @@ void do_tick(int my_cpu)
     if (update_spinner) {
         spin_idx = (spin_idx + 1) % NUM_SPIN_STATES;
         display_spinner(spin_state[spin_idx]);
+        if (enable_sound) {
+            sound_tick_task();
+        }
     }
 
     // This only tick one time per second
