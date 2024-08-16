d9deb248d7e88d1e34273adb33034c7441b85664
Chao Li <lichao@loongson.cn>
2024-08-16 10:11:05 +0800

    system/loongarch: Add LoongArch ARCH specific files
    Added LoongArch ARCH specific files: cpuid.c, cpuinfo.c, hwctrl.c,
    memctrl.c, temperature.c, vmem.c, registers.h
    
    They use the same pubilc API for i386 and x64 platforms.
    
    Signed-off-by: Chao Li <lichao@loongson.cn>

---
+++ system/loongarch/memctrl.c
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: GPL-2.0
+// Copyright (C) 2004-2023 Sam Demeulemeester
+// Copyright (C) 2024 Loongson Technology Corporation Limited. All rights reserved.
+//
+// ------------------------
+//
+// Platform-specific code for IMC configuration, ECC support, etc.
+//
+
+#include <stdbool.h>
+
+#include "config.h"
+#include "cpuinfo.h"
+
+#include "memctrl.h"
+#include "imc/imc.h"
+
+#include "display.h"
+
+imc_info_t imc = {"UNDEF", 0, 0, 0, 0, 0, 0, 0, 0};
+
+ecc_info_t ecc_status = {false, ECC_ERR_NONE, 0, 0, 0, 0};
+
+// ---------------------
+// -- Public function --
+// ---------------------
+
+void memctrl_init(void)
+{
+    ecc_status.ecc_enabled = false;
+
+    if (!enable_mch_read) {
+        return;
+    }
+
+    switch(imc.family) {
+      case IMC_LA464:
+      case IMC_LA664:
+        get_imc_config_loongson_ddr4();
+        break;
+      default:
+        break;
+    }
+}
+
+void memctrl_poll_ecc(void)
+{
+    if (!ecc_status.ecc_enabled) {
+        return;
+    }
+}
