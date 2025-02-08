fac0e0b883e19a030891261cbf17e6c855310507
01e3 <01e3@ans.pl>
2025-02-08 15:08:21 -0800

    Avoid unnecessary screen writes
    Calling clear_screen_region to erase the screen section and then
    immediately calling printf/prints to write the new information is
    sub-optimal as the screen gets updated twice.
    
    This is especially expensive when Frame Buffer is used.
    
    This also creates an unpleasant flickering effect with quickly changing
    information, like test patterns.
    
    Introduce printf_region and prints_region macros to call printf / prints
    and then clear_screen_region to clear only the region outside the printed
    area.
    
    For consistency, also convert all display_cpu_topo_* macros.
    
    Next, avoid redundant screen clearing in display_start_test:
     - display_test_number already takes care of padding,
     - test description gets updated immediately after, no need to clear
       that region.
    
    Finally, simplify the code by adding SCREEN_MAX_COL to define the maximum
    column of the screen, so it can be used instead of SCREEN_WIDTH - 1.

--- app/x86/interrupt.c
+++ app/x86/interrupt.c
@@ -247,8 +247,7 @@ ISR_GP_REGS_ONLY void interrupt(struct trap_regs *trap_regs)
         display_pinned_message(11, 7 + 3*i, "%02x", (uintptr_t)pp[i]);
     }
 
-    clear_screen_region(ROW_FOOTER, 0, ROW_FOOTER, SCREEN_WIDTH - 1);
-    prints(ROW_FOOTER, 0, "Press any key to reboot...");
+    printf_region(ROW_FOOTER, 0, SCREEN_MAX_COL, "Press any key to reboot...");
 
     while (get_key() == 0) { }
     reboot();
