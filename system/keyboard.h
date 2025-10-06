c7234a38e61400b876e5ee40ae0162c7492b013a
Martin Whitaker <memtest@martin-whitaker.me.uk>
2025-10-06 10:37:00 +0100

    Update keyboard.h to document cursor key mapping.

--- system/keyboard.h
+++ system/keyboard.h
@@ -40,7 +40,8 @@ void keyboard_init(void);
 /**
  * Checks if a key has been pressed and returns the primary ASCII character
  * corresponding to that key if so, otherwise returns the null character.
- * F1 to F10 are mapped to the corresponding decimal digit (F10 -> 0). All
+ * F1 to F10 are mapped to the corresponding decimal digit (F10 -> 0). The
+ * Up, Down, Left, Right cursor keys are mapped to 'u', 'd', 'l', 'r'. All
  * other keys that don't have a corresponding ASCII character are ignored.
  * Characters are only returned for key presses, not key releases. A US
  * keyboard layout is assumed (because we can't easily do anything else).
